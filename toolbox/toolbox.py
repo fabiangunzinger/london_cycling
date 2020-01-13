9f5d6e9d7d3e894b0f060d40a1118d67e80ae2a3
fabiangunzinger <fa.gunzinger@gmail.com>
2020-01-13 19:24:54 +0000

    Clearing up data loading function

--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -1,19 +1,24 @@
-
+import glob
+import janitor
+import pandas as pd
+import numpy as np
 
 def make_sample_data():
 	"""
 	Creates a 1 percent sample of the 2016 cycle hires data.
 	"""
+
 	files = glob.glob('./data/2016TripData/*.csv')
-	
-	# Read and clean data
+	frames= [pd.read_csv(f) for f in files]
+	drop_vars = 'unnamed_ | _logical_terminal | endstationpriority_'
+
 	df = (
-	    pd.concat([pd.read_csv(f) for f in files], ignore_index=True, sort=False)
+	    pd.concat(frames, ignore_index=True, sort=False)
 	    .clean_names()
+	    .remove_columns(list(df.filter(regex=drop_vars)))
+	    .transform_column('duration', lambda x: x/60)
 	)
 
-	# Drop irrelevant columns
-	df.drop(list(df.filter(regex='unnamed|logical|priority')), axis = 1, inplace = True)
-	
 	df.sample(frac = 0.1).to_csv('./data/sample2016.csv', index=False)
+	return 'Sample saved as ./data/sample2016.csv'
 
